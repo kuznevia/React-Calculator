896b8c43ac31628654d983a22ea9a9d34ddfb264
Viacheslav Kuznetsov <via.kuznetsov@gmail.com>
2022-04-12 16:23:07 +0300

    adding plugin jest try 3

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,11 +1,11 @@
+import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('testing sum, 2+2+4', () => {
   render(<App />);
-  const inputField = screen.getByTestId(/inputBox/i);
   fireEvent.change(screen.getByTestId(/inputBox/i), {
-    target: {value: '2+2+4'},
+    target: { value: '2+2+4' },
   });
   fireEvent.click(screen.getByText(/=/i));
   const resultField = screen.getByTestId(/resultBox/i);
