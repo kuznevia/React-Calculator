af0635c7bc46e44a41ed40d412453e6948c03120
Viacheslav Kuznetsov <via.kuznetsov@gmail.com>
2022-04-12 16:25:42 +0300

    adding new tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -11,3 +11,63 @@ test('testing sum, 2+2+4', () => {
   const resultField = screen.getByTestId(/resultBox/i);
   expect(resultField.textContent).toBe('8');
 });
+
+test('testing sub, 2+6-10', () => {
+  render(<App />);
+  fireEvent.change(screen.getByTestId(/inputBox/i), {
+    target: { value: '2+6-10' },
+  });
+  fireEvent.click(screen.getByText(/=/i));
+  const resultField = screen.getByTestId(/resultBox/i);
+  expect(resultField.textContent).toBe('-2');
+});
+
+test('testing multiplication, 3*6*2', () => {
+  render(<App />);
+  fireEvent.change(screen.getByTestId(/inputBox/i), {
+    target: { value: '3*6*2' },
+  });
+  fireEvent.click(screen.getByText(/=/i));
+  const resultField = screen.getByTestId(/resultBox/i);
+  expect(resultField.textContent).toBe('36');
+});
+
+test('testing division, 3*6/2', () => {
+  render(<App />);
+  fireEvent.change(screen.getByTestId(/inputBox/i), {
+    target: { value: '3*6/2' },
+  });
+  fireEvent.click(screen.getByText(/=/i));
+  const resultField = screen.getByTestId(/resultBox/i);
+  expect(resultField.textContent).toBe('9');
+});
+
+test('testing subsequence, 2+2*2', () => {
+  render(<App />);
+  fireEvent.change(screen.getByTestId(/inputBox/i), {
+    target: { value: '2+2*2' },
+  });
+  fireEvent.click(screen.getByText(/=/i));
+  const resultField = screen.getByTestId(/resultBox/i);
+  expect(resultField.textContent).toBe('6');
+});
+
+test('testing parentheses, (2+2)*(2+2)', () => {
+  render(<App />);
+  fireEvent.change(screen.getByTestId(/inputBox/i), {
+    target: { value: '(2+2)*(2+2)' },
+  });
+  fireEvent.click(screen.getByText(/=/i));
+  const resultField = screen.getByTestId(/resultBox/i);
+  expect(resultField.textContent).toBe('16');
+});
+
+test('testing complex, 3*(2+2)/12-66+(23*12)', () => {
+  render(<App />);
+  fireEvent.change(screen.getByTestId(/inputBox/i), {
+    target: { value: '3*(2+2)/12+66-(23*12)' },
+  });
+  fireEvent.click(screen.getByText(/=/i));
+  const resultField = screen.getByTestId(/resultBox/i);
+  expect(resultField.textContent).toBe('211');
+});
