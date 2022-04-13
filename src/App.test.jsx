58be97d34a2b808449d9483dc1612d0abea0c4aa
Viacheslav Kuznetsov <via.kuznetsov@gmail.com>
2022-04-13 11:55:28 +0300

    optimizing tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,74 +6,50 @@ beforeEach(() => {
   render(<App />);
 });
 
-test('testing sum, 2+2+4', () => {
+const runCalc = (value) => {
   fireEvent.change(screen.getByTestId(/inputBox/i), {
-    target: { value: '2+2+4' },
+    target: { value },
   });
   fireEvent.click(screen.getByText(/=/i));
-  const resultField = screen.getByTestId(/resultBox/i);
+  return screen.getByTestId(/resultBox/i);
+};
+
+test('testing sum, 2+2+4', () => {
+  const resultField = runCalc('2+2+4');
   expect(resultField.textContent).toBe('8');
 });
 
 test('testing sub, 2+6-10', () => {
-  fireEvent.change(screen.getByTestId(/inputBox/i), {
-    target: { value: '2+6-10' },
-  });
-  fireEvent.click(screen.getByText(/=/i));
-  const resultField = screen.getByTestId(/resultBox/i);
+  const resultField = runCalc('2+6-10');
   expect(resultField.textContent).toBe('-2');
 });
 
 test('testing multiplication, 3*6*2', () => {
-  fireEvent.change(screen.getByTestId(/inputBox/i), {
-    target: { value: '3*6*2' },
-  });
-  fireEvent.click(screen.getByText(/=/i));
-  const resultField = screen.getByTestId(/resultBox/i);
+  const resultField = runCalc('3*6*2');
   expect(resultField.textContent).toBe('36');
 });
 
 test('testing division, 3*6/2', () => {
-  fireEvent.change(screen.getByTestId(/inputBox/i), {
-    target: { value: '3*6/2' },
-  });
-  fireEvent.click(screen.getByText(/=/i));
-  const resultField = screen.getByTestId(/resultBox/i);
+  const resultField = runCalc('3*6/2');
   expect(resultField.textContent).toBe('9');
 });
 
 test('testing subsequence, 2+2*2', () => {
-  fireEvent.change(screen.getByTestId(/inputBox/i), {
-    target: { value: '2+2*2' },
-  });
-  fireEvent.click(screen.getByText(/=/i));
-  const resultField = screen.getByTestId(/resultBox/i);
+  const resultField = runCalc('2+2*2');
   expect(resultField.textContent).toBe('6');
 });
 
 test('testing parentheses, (2+2)*(2+2)', () => {
-  fireEvent.change(screen.getByTestId(/inputBox/i), {
-    target: { value: '(2+2)*(2+2)' },
-  });
-  fireEvent.click(screen.getByText(/=/i));
-  const resultField = screen.getByTestId(/resultBox/i);
+  const resultField = runCalc('(2+2)*(2+2)');
   expect(resultField.textContent).toBe('16');
 });
 
 test('testing parentheses option 2, (2+2)*2', () => {
-  fireEvent.change(screen.getByTestId(/inputBox/i), {
-    target: { value: '(2+2)*2' },
-  });
-  fireEvent.click(screen.getByText(/=/i));
-  const resultField = screen.getByTestId(/resultBox/i);
+  const resultField = runCalc('(2+2)*2');
   expect(resultField.textContent).toBe('8');
 });
 
 test('testing complex, 3*(2+2)/12-66+(23*12)', () => {
-  fireEvent.change(screen.getByTestId(/inputBox/i), {
-    target: { value: '3*(2+2)/12-66+(23*12)' },
-  });
-  fireEvent.click(screen.getByText(/=/i));
-  const resultField = screen.getByTestId(/resultBox/i);
+  const resultField = runCalc('3*(2+2)/12-66+(23*12)');
   expect(resultField.textContent).toBe('211');
 });
