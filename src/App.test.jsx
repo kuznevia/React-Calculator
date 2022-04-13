61b78b87a543c2e71be3b0e65572824fd868a480
Viacheslav Kuznetsov <via.kuznetsov@gmail.com>
2022-04-13 11:44:53 +0300

    fixing bug

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -7,6 +7,9 @@ beforeEach(() => {
 });
 
 test('testing sum, 2+2+4', () => {
+  fireEvent.change(screen.getByTestId(/inputBox/i), {
+    target: { value: '2+2+4' },
+  });
   fireEvent.click(screen.getByText(/=/i));
   const resultField = screen.getByTestId(/resultBox/i);
   expect(resultField.textContent).toBe('8');
